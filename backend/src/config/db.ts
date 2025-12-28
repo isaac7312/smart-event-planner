8dfc0a5edfa9e4f0bcaf2d3e1788b74518814997
isaac7312 <issacpreetham57@gmail.com>
2025-12-28 23:54:58 +0530

    fix db config for railway

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,11 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST!,
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASSWORD!,
+  database: process.env.DB_NAME!,
 });
 
 db.connect((err) => {
@@ -20,4 +19,3 @@ db.connect((err) => {
 });
 
 export default db;
-
