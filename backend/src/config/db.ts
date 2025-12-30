986f84898524d96153b24fad61476c9a5e702047
dharshinikamaraj11-maker <dharshinikamaraj11@gmail.com>
2025-12-30 10:27:44 +0530

    Refactor MySQL connection to use config object

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createConnection(process.env.MYSQL_URL!);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 db.connect((err) => {
   if (err) {
@@ -14,3 +19,5 @@ db.connect((err) => {
 });
 
 export default db;
+
+
